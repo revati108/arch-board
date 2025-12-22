adbefd1de916a1a64136899186c9a489c898cb26
shashstormer <shashanka5398@gmail.com>
2025-12-22 17:35:13 +0530

    Add Linux username retrieval and versioning support in config

--- utils/config.py
+++ utils/config.py
@@ -2,6 +2,22 @@ from xtracto import Config
 import dotenv
 import os
 import time
+from version_info import VERSION
+import pwd
+
+
+def get_linux_username():
+    """
+    Retrieves the actual login name of the current process owner on Linux.
+    """
+    # os.getuid() gets the User ID (UID) of the current process
+    uid = os.getuid()
+    # pwd.getpwuid() gets a password database entry (struct_passwd object) for that UID
+    user_info = pwd.getpwuid(uid)
+    # The username is stored in the pw_name attribute (or index 0)
+    return user_info.pw_name
+
+
 
 dotenv.load_dotenv()
 config = Config()
@@ -12,8 +28,8 @@ def get_context(overrides: dict = None):
         "page_description": "Arch Board, simplest way to control your system.",
         "page_header": "Dashboard",
         "production": str(config.production).lower(),
-        "username": os.environ.get("USER", "user"),
-        "cache_key": os.environ.get("VERSION", "0.0.0") if config.production else str(time.time()),
+        "username": os.environ.get("USER", get_linux_username()),
+        "cache_key": VERSION if config.production else str(time.time()),
     }
     if overrides:
         context.update(overrides)
